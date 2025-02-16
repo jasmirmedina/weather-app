ec220f2cda37e6d1a2b1cb719ab382d6a0856916
jasmirmedina <jasmirmedina56@gmail.com>
2025-02-16 01:00:18 -0600

    feat: add lucide icons

---
+++ src/scripts/create-icons.js
@@ -0,0 +1,15 @@
+import { createIcons, Wind, WindArrowDown, CloudFog, CloudRain } from "lucide";
+
+createIcons({
+  icons: {
+    Wind,
+    WindArrowDown,
+    CloudFog,
+    CloudRain,
+  },
+  attrs: {
+    stroke: "#ffffff99",
+    width: 20,
+    height: 20,
+  },
+});
